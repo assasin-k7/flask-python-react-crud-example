a21cded8f020344e5f1f443434fcc1a3f7dfb53d
k7 <48987367+assasin-k7@users.noreply.github.com>
2025-01-16 05:26:15 -0500

    create/service.py

--- app/repository/mongo.py
+++ app/repository/mongo.py
@@ -2,7 +2,6 @@ import os
 from pymongo import MongoClient
 
 COLLECTION_NAME = 'kudos'
-
 class MongoRepository(object):
     def __init__(self):
         mongo_url = os.environ.get('MONGO_URL')
@@ -14,6 +13,16 @@ class MongoRepository(object):
     def create(self, kudo):
         return self.db.kudos.insert_one(kudo)
     def update(self, selector, kudo):
-        return self.db.kudos.replace_one(selector, kudo).modified_count 
+        return self.db.kudos.replace_one(selector, kudo).modified_count
     def delete(self, selector, kudo):
-        return self.db.kudos.delete_one(selector).deleted_count   
+        return self.db.kudos.delete_one(selector).deleted_count
+# from pymongo import MongoClient
+# from pymongo.errors import ConnectionFailure
+# try:
+#     client = MongoClient("mongodb://localhost:27017/")
+#     client.admin.command('ping')
+#     print("Successfully connected to MongoDB!")
+# except ConnectionFailure as e:
+#     print(f"Could not connect to MongoDB: {e}")
+# finally:
+#     client.close()    
